7447071806e314597cc09c2749ad51d7c21c0e7d
root <root@xbluccnjbe.local>
2026-01-19 17:09:37 +0000

    Add cache-busting for JS script

--- wordpress/themes/twentytwentyfive-child/functions.php
+++ wordpress/themes/twentytwentyfive-child/functions.php
@@ -10,7 +10,10 @@ function twentytwentyfive_child_enqueue_styles() {
     // Подключаем стили дочерней темы
     wp_enqueue_style('child-style', get_stylesheet_uri(), array('parent-style'), wp_get_theme()->get('Version'));
 
-    // Подключаем скрипт для хедера
-    wp_enqueue_script('oasis-header-js', get_stylesheet_directory_uri() . '/assets/oasis-header.js', array(), wp_get_theme()->get('Version'), true);
+    // Подключаем скрипт для хедера с cache-busting
+    $js_file = get_stylesheet_directory() . '/assets/oasis-header.js';
+    $js_version = file_exists($js_file) ? filemtime($js_file) : '1.0';
+    wp_enqueue_script('oasis-header-js', get_stylesheet_directory_uri() . '/assets/oasis-header.js', array(), $js_version, true);
+
 }
 add_action('wp_enqueue_scripts', 'twentytwentyfive_child_enqueue_styles');
